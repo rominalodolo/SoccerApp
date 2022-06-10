aa84f0bb8b69bf71a0062f94ab1855b35f26be5d
Romina <83961643+rominalodolo@users.noreply.github.com>
2022-06-10 14:43:40 +0200

    Update Teams.java

--- SoccerLeaderBoard/src/Teams.java
+++ SoccerLeaderBoard/src/Teams.java
@@ -1,8 +1,28 @@
-public enum Teams {
-    Tarantulas2,
-    Lions,
-    FCAwesome,
-    МanchesterUnited,
-    Liverpool,
-    Grouches,
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+public class Teams {
+
+    public static void String Teams throws IOExcepton {
+
+        Stream<String> rows5 = Files.lines(Paths.get("/Users/rominalodolo/Desktop/GitHub/JAVA/SoccerApp/SoccerLeaderBoard/src/data.txt"));
+        Map<String, Integer> map = new HashMap<>();
+        map = rows5
+                .map(x -> x.split(" ,"))
+                .
+        List<Team> Teams = new ArrayList<>();
+
+        teams.add(new Team())
+        String[] team = {"Tarantulas2", "Lions", "FCAwesome", "МanchesterUnited", "Liverpool", "Grouches"};
+        Arrays.stream(team)
+                .sorted()
+                .ifPresent();
+    }
 }
+
+//public boolean contains(Arrays team) throws ClassCastException {
+//        Arrays.asList("Tarantulas2", "Lions", "FCAwesome", "МanchesterUnited", "Liverpool", "Grouches");
